6954de22ca23931ebe92e8870ff710e94f323961
Nepse praz <115599991+nepsepraz@users.noreply.github.com>
2022-10-12 14:35:15 +1100

    updated comments
    Just added comments to make code easy to read

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import turtle
 import pandas
 
+#screen setup
 screen = turtle.Screen()
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+
+#state guessed list
 states_guessed  = []
+#count number of states guessed
 states_count = 0
+#read csv data
 data = pandas.read_csv("50_states.csv")
+#Converting CSV data to list
 state = data.state.to_list()
 while states_count < 50:
     answer = screen.textinput(title=f"{states_count}/50 states", prompt="What is the another States Name??").title()
+    #giving exit feature to user
     if answer == "Exit":
         for states in states_guessed:
             state.remove(states)
         df = pandas.DataFrame(state)
         df.to_csv("States to learn.csv")
         break
+     #answer checking
     if answer in state:
         answer_row = data[data.state == answer]
         tim = turtle.Turtle()
